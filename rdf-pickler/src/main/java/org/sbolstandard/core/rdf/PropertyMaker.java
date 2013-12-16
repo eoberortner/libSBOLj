c64cf71a3c0463f1f4c9a5be2aea9a108e65ae28
= <turingatemyhamster@gmail.com>
2013-12-16 23:55:31 +0000

    Javadocs for PropertyMaker.

--- rdf-pickler/src/main/java/org/sbolstandard/core/rdf/PropertyMaker.java
+++ rdf-pickler/src/main/java/org/sbolstandard/core/rdf/PropertyMaker.java
@@ -4,11 +4,23 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
 
 /**
- * Strategy for making a property from a property name.
+ * Strategy for making a stable property.
+ *
+ * <p>
+ *   Each instance of this class is responsible for a single property which is potentially made in multiple models.
+ *   It will typically store the name or URI of the property as state and then mint a new
+ *   {@link com.hp.hpl.jena.rdf.model.Property} for each {@link com.hp.hpl.jena.rdf.model.Model}.
+ * </p>
  *
  * @author Matthew Pocock
  */
 public interface PropertyMaker {
+  /**
+   * Return a property.
+   *
+   * @param model   the model that will contain the property
+   * @return        the property in {@code model}
+   */
   public Property propertyFor(Model model);
 
   public static final class ByName implements PropertyMaker {

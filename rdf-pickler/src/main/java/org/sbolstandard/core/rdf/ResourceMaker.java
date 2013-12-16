a5f2812018ec65c48e07d2058a138755194ef23c
= <turingatemyhamster@gmail.com>
2013-12-16 23:31:49 +0000

    Documented ResourceMaker.

--- rdf-pickler/src/main/java/org/sbolstandard/core/rdf/ResourceMaker.java
+++ rdf-pickler/src/main/java/org/sbolstandard/core/rdf/ResourceMaker.java
@@ -10,8 +10,30 @@ import org.sbolstandard.core.SBOLObject;
  * @author Matthew Pocock
  */
 public interface ResourceMaker<R> {
+  /**
+   * Return a resource for {@code r}.
+   *
+   * <p>
+   *   Successive calls for equivalent {@code r} should return equivalent resources.
+   * </p>
+   *
+   * <p>
+   *   Implementations are free to cache resources. However, they should be careful not to retain a hard reference to
+   *   the {@code model}.
+   * </p>
+   *
+   * @param model   the model to contain the resource
+   * @param r       the object to find the resource for
+   * @return        a resource which represents {@code r}
+   */
   public Resource asResource(Model model, R r);
 
+  /**
+   * An implementation of {@link org.sbolstandard.core.rdf.ResourceMaker} that uses
+   * {@link org.sbolstandard.core.SBOLObject#getURI()}.
+   *
+   * @author Matthew Pocock
+   */
   public static final class FromIdentity implements ResourceMaker<SBOLObject> {
     @Override
     public Resource asResource(Model model, SBOLObject r) {
